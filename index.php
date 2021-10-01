c6888e0694b48a1f59c4194868923868eac6529a
mylittleunicorn <nasir.dev48@gmail.com>
2021-10-01 15:11:15 +0700

    aaaa

---
+++ index.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    
+    <!-- css -->
+    <link rel="stylesheet" href="style.css">
+
+
+    <link rel="stylesheet" href="jquery.flex-images.css">
+
+    
+    <title>Free Image</title>
+  </head>
+  <body>
+      <!-- NAV -->
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">FREE IMAGE</a> 
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <!-- Left nav -->
+          <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Left</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">Codeply</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+            </ul>
+          </div>
+
+          <!-- Right Nav -->
+          <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Right</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="#"> UPLOAD<i class="fa fa-upload mx-md-1" aria-hidden="true"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- NAV -->
+
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Fluid jumbotron</h1>
+          <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        </div>
+      </div>
+
+      <div class="container text-center">
+        <div class="flex-images">
+          <div class="item" width="100"data-w="200" data-h="150"><img src="image/8.jpg"></div>
+          <div class="item" data-w="250" data-h="150"><img src="image/2.jpg"></div>
+          <div class="item" data-w="150" data-h="150"><img src="image/3.jpg"></div>
+          <div class="item" data-w="150" data-h="150"><img src="image/4.jpg"></div>
+          <div class="item" data-w="200" data-h="150"><img src="image/5.jpg"></div>
+          <div class="item" data-w="100" data-h="150"><img src="image/6.jpg"></div>
+          <div class="item" data-w="180" data-h="150"><img src="image/7.jpg"></div>
+          <div class="item" data-w="185" data-h="150"><img src="image/8.jpg"></div>
+          <div class="item" data-w="250" data-h="150"><img src="image/2.jpg"></div>
+          <div class="item" data-w="150" data-h="150"><img src="image/3.jpg"></div>
+          <div class="item" data-w="150" data-h="150"><img src="image/4.jpg"></div>
+          <div class="item" data-w="200" data-h="150"><img src="image/5.jpg"></div>
+          <div class="item" data-w="100" data-h="150"><img src="image/6.jpg"></div>
+          <div class="item" data-w="180" data-h="150"><img src="image/7.jpg"></div>
+          <div class="item" data-w="185" data-h="150"><img src="image/8.jpg"></div>
+          <div class="item" data-w="350" data-h="150"><img src="image/9.jpg"></div>
+          <div class="item" data-w="200" data-h="150"><img src="image/10.jpg"></div>
+          <div class="item" data-w="165" data-h="150"><img src="image/11.jpg"></div>
+          <div class="item" data-w="150" data-h="150"><img src="image/12.jpg"></div>
+          <div class="item" data-w="120" data-h="150"><img src="image/13.jpg"></div>
+          <div class="item" data-w="120" data-h="150"><img src="image/14.jpg"></div>
+          <div class="item" data-w="120" data-h="150"><img src="image/20.jpg"></div>
+          <div class="item" data-w="180" data-h="150"><img src="image/7.jpg"></div>
+          <div class="item" data-w="185" data-h="150"><img src="image/8.jpg"></div>
+          <div class="item" data-w="350" data-h="150"><img src="image/9.jpg"></div>
+          <div class="item" data-w="200" data-h="150"><img src="image/10.jpg"></div>
+          <div class="item" data-w="165" data-h="150"><img src="image/11.jpg"></div>
+          <div class="item" data-w="150" data-h="150"><img src="image/12.jpg"></div>
+          <div class="item" data-w="120" data-h="150"><img src="image/13.jpg"></div>
+          <div class="item" data-w="120" data-h="150"><img src="image/14.jpg"></div>
+          <div class="item" width="10" data-w="120" data-h="150"><img src="image/20.jpg"></div>
+        </div>
+        </div>
+      
+    
+        
+
+    <!-- Optional JavaScript; choose one of the two! -->  
+    
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="jquery.flex-images.js"></script>
+    <script type="text/javascript">
+          $('.flex-images').flexImages({rowHeight: 100});
+        </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+    
+  </body>
+</html>
